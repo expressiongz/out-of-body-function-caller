2d4c524da284ccf2f9e6106a6a48b2ad519b2a17
expression <97218884+expressiongz@users.noreply.github.com>
2022-08-16 22:10:25 +0100

    formatted, improved

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,11 +1,10 @@
-
 #include <iostream>
 #include <Windows.h>
 #include <thread>
 #include <vector>
 #include <tuple>
 
-typedef void(__stdcall* cextf_proto)(const std::uint32_t, const std::uint32_t);
+typedef void( __stdcall* cextf_proto )( const std::uint32_t, const std::uint32_t );
 
 enum calling_conventions : std::uint8_t
 {
@@ -15,86 +14,94 @@ enum calling_conventions : std::uint8_t
 };
 
 
-template<typename func_ret_t, calling_conventions ccv, typename... function_args>
-void __stdcall call_ext_func(const std::uint32_t function_address, const std::uint32_t tuple_addr)
+template< typename func_ret_t, calling_conventions ccv, typename... function_args >
+void __stdcall call_ext_func( std::uint32_t function_address, std::uint32_t tuple_addr )
 {
-    const auto function_args_tuples = *reinterpret_cast<std::tuple<function_args...>*>(tuple_addr);
+    const auto function_args_tuples = *reinterpret_cast< std::tuple< function_args... > * >( tuple_addr );
 
-    switch (ccv)
+    switch ( ccv )
     {
 		    case calling_conventions::stdcall_:
 		    {
-			      typedef func_ret_t(__stdcall* stdcall_proc_func_proto)(function_args...);
-			      const auto stdcall_proc_func = reinterpret_cast<stdcall_proc_func_proto>(function_address);
-			      std::apply(stdcall_proc_func, function_args_tuples);
-			       break;
+			      typedef func_ret_t( __stdcall* stdcall_proc_func_proto )( function_args... );
+			      const auto stdcall_proc_func = reinterpret_cast< stdcall_proc_func_proto >( function_address );
+			      std::apply( stdcall_proc_func, function_args_tuples );
+			      break;
 		    }
 
 		    case calling_conventions::cdecl_:
 		    {
-			      typedef func_ret_t(__cdecl* cdecl_proc_func_proto)(function_args...);
-			      const auto cdecl_proc_func = reinterpret_cast<cdecl_proc_func_proto>(function_address);
-			      std::apply(cdecl_proc_func, function_args_tuples);
+			      typedef func_ret_t( __cdecl* cdecl_proc_func_proto )( function_args... );
+			      const auto cdecl_proc_func = reinterpret_cast< cdecl_proc_func_proto >( function_address );
+			      std::apply( cdecl_proc_func, function_args_tuples );
 			      break;
-         }
-		     case calling_conventions::fastcall_:
-		     {
-			      typedef func_ret_t(__fastcall* fastcall_proc_func_proto)(function_args...);
-			      const auto fastcall_proc_func = reinterpret_cast<fastcall_proc_func_proto>(function_address);
-			      std::apply(fastcall_proc_func, function_args_tuples);
+			}
+		    case calling_conventions::fastcall_:
+		    {
+			      typedef func_ret_t( __fastcall* fastcall_proc_func_proto )( function_args... );
+			      const auto fastcall_proc_func = reinterpret_cast< fastcall_proc_func_proto >( function_address );
+			      std::apply( fastcall_proc_func, function_args_tuples );
 			      break;
-		      }
+		    }
     }
 }
 
-std::vector<std::uint8_t> read_func(void* function_address)
+std::vector< std::uint8_t > ret_function_instrs( void* function_address )
 {
-    std::vector<std::uint8_t> bytes_read{};
-    auto const* curr_byte = static_cast<std::uint8_t*>(function_address);
-    do
-    {
-        bytes_read.push_back(*curr_byte);
-        curr_byte += 1;
-
-    } while (*(curr_byte + 1) != 0xCC || *(curr_byte + 2) != 0xCC || *(curr_byte + 3) != 0xCC);
-    return bytes_read;
+	std::vector< std::uint8_t > function_bytes{ };
+	auto byte = static_cast< std::uint8_t* >( function_address );
+	do 
+	{
+		function_bytes.push_back( *byte );
+		++byte;
+	} while ( *reinterpret_cast< std::uint32_t* >( byte ) != 0xCCCCCCCC );
+	return function_bytes;
 }
 
 
 void* alloc_func(void* func_address)
 {
-    const auto func_bytes = read_func(func_address);
-    const auto funct_base_addr = VirtualAlloc(nullptr, func_bytes.size(), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-    std::memcpy(funct_base_addr, func_bytes.data(), func_bytes.size());
-    return funct_base_addr;
+    const auto function_instructions = ret_function_instrs( func_address );
+    const auto allocated_function = VirtualAlloc( nullptr, function_instructions.size( ), MEM_COMMIT, PAGE_EXECUTE_READWRITE );
+
+	if( !allocated_function )
+		throw std::runtime_error( "failed to allocate memory for function." );
+
+    std::memcpy( allocated_function, function_instructions.data( ), function_instructions.size( ) );
+    return allocated_function;
 }
 
 
-template<typename... tuple_el>
-void* alloc_tuple(std::tuple<tuple_el...> tuple)
+template< typename... tuple_el >
+void* alloc_tuple( std::tuple< tuple_el... > tuple )
 {   
-    const auto tuple_addr = VirtualAlloc(nullptr, sizeof(tuple), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
-    std::memcpy(tuple_addr, &tuple, sizeof(tuple));
-    return tuple_addr;
+    const auto allocated_tuple = VirtualAlloc( nullptr, sizeof( tuple ), MEM_COMMIT, PAGE_EXECUTE_READWRITE );
+	if( !allocated_tuple )
+		throw std::runtime_error( "failed to allocate memory for tuple" );
+
+    std::memcpy( allocated_tuple, &tuple, sizeof( tuple ) );
+    return allocated_tuple;
 }
 
 
-void main_thread()
+void main_thread( HMODULE dll_module )
 {
-    // imagine we are calling a print function that takes 1 string argument.
-    constexpr std::tuple tpl{ "\n\nexprssnware is me\n"};
-    const auto tuple_addr = alloc_tuple(tpl);
-    const auto func_addr = alloc_func(reinterpret_cast<void*>(call_ext_func<void, calling_conventions::cdecl_, const char*>));
+	const auto base = reinterpret_cast< std::uint32_t >( GetModuleHandleA( nullptr ) );
+	constexpr auto function_rva = 0x1000;
 
-    const auto call_game_func = static_cast<cextf_proto>(func_addr);
-    call_game_func(0x003B1000, reinterpret_cast<std::uint32_t>(tuple_addr));
+    constexpr auto arg_tuple = std::make_tuple< const char* >( { "exprssn" } );
+	const auto tuple = alloc_tuple( arg_tuple );
+
+    const auto caller_func_address = alloc_func( reinterpret_cast< void* >( call_ext_func< void, calling_conventions::cdecl_, std::string_view > ) );
+	const auto caller = static_cast< cextf_proto >( caller_func_address );
+
+    caller( base + function_rva, reinterpret_cast< std::uint32_t >( tuple ) );
+	FreeLibrary( dll_module );
 }
 
-bool __stdcall DllMain(HMODULE dll_module, const std::uint32_t reason_for_call, void* reserved)
+bool __stdcall DllMain( HMODULE dll_module, const std::uint32_t reason_for_call, void*  )
 {
-    if (reason_for_call == DLL_PROCESS_ATTACH)
-    {
-        std::thread{ &main_thread }.detach();
-    }
+    if ( reason_for_call == DLL_PROCESS_ATTACH )
+        std::thread{ &main_thread, dll_module }.detach( );
     return true;
 }
